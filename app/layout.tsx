87a973d31acf1dc323488d6b7ef0a5fb4bb7d3e1
michaelabando17GH <michael.abando@gmail.com>
2026-05-21 19:11:51 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'DO NOT RENT. OWN. | KAPWA OFW',
+  description: 'He\'s rich because we rent. We\'re poor because we don\'t own. KAH-YAH-WON',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
